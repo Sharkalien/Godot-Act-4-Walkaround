2e703c831dde492d68c048a91a84120074fc9b68
Sharkalien <leonardodanielescoto@gmail.com>
2024-12-11 22:01:44 -0600

    Update Sylladex Prototype.gd
    Using tweens were a bust. Works exactly like in the flash now
    
    Also need to remember to set the cameras' process mode to physics in every scene for smoothness

--- ui/sylladex/Sylladex Prototype.gd
+++ ui/sylladex/Sylladex Prototype.gd
@@ -1,22 +1,25 @@
 extends Node2D
 
-onready var hud = $"."
+onready var hud = self
 var sylladex := []
 var card = load("res://ui/sylladex/Card.tscn")
 var tCard
 #var queueStack = 0
 #var cardIndex = 0
-var cardPos
+var cardPos:Vector2
+var using:bool = false
 
+const numOfStacks = 4
+const numOfCards = 6
 
 func _ready() -> void:
 	var queueStack = 0
 	var cardIndex = 0
 	var zIndex = 0
-	while queueStack < 4:
+	while queueStack < numOfStacks:
 		sylladex.push_back([])
 		cardIndex = 0
-		while cardIndex < 6:
+		while cardIndex < numOfCards:
 			tCard = card.instance()
 			hud.add_child(tCard)
 #			tCard.show_behind_parent = true
@@ -31,15 +34,21 @@ func _ready() -> void:
 		queueStack += 1
 
 
+func _process(_delta: float) -> void:
+	toggleSyl()
+
 func _on_TextureButton_toggled(button_pressed: bool) -> void:
+	using = button_pressed
+
+func toggleSyl():
 	var queueStack = 0
 	var cardIndex = 0
-	while queueStack < 4:
+	while queueStack < numOfStacks:
 		cardIndex = 0
-		while cardIndex < 6:
+		while cardIndex < numOfCards:
 			tCard = sylladex[queueStack][cardIndex]
 			cardPos = Vector2.ZERO
-			if button_pressed:
+			if using:
 				cardPos.x = 5 + 20 * queueStack + 25 * cardIndex
 				cardPos.y = 20 + 75 * queueStack + 15 * cardIndex
 			else:
@@ -50,11 +59,11 @@ func _on_TextureButton_toggled(button_pressed: bool) -> void:
 				tCard.position.x = cardPos.x
 				tCard.position.y = cardPos.y
 			else:
-				create_tween().set_trans(Tween.TRANS_QUAD).tween_property(tCard,"global_position",cardPos,0.06)
+#				create_tween().set_trans(Tween.TRANS_QUAD).tween_property(tCard,"global_position",cardPos,0.06)
 #				create_tween().tween_property(tCard,"position:x",ceil((cardPos.x - tCard.position.x) / 2),0.03)
 #				create_tween().tween_property(tCard,"position:y",ceil((cardPos.y - tCard.position.y) / 2),0.03)
-#				tCard.position.x += ceil((cardPos.x - tCard.position.x) / 2)
-#				tCard.position.y += ceil((cardPos.y - tCard.position.y) / 2)
+				tCard.position.x += ceil((cardPos.x - tCard.position.x) / 2)
+				tCard.position.y += ceil((cardPos.y - tCard.position.y) / 2)
 #			else:
 #				tCard.position.x = cardPos.x
 #				tCard.position.y = cardPos.y
